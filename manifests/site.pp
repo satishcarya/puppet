5971aaf3afa5df83804410c52c5e03461a568a1d
root <root@gitpractice.c.steam-canto-775.internal>
2018-06-18 08:39:34 +0000

    fixed duplicate entries in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,162 +1,90 @@
 node gitpractice-2 {
-
-file {'/etc/motd':
-	content => ' This is gitpractice 2 system',
-	}
-
-	service {'postfix':
-	ensure => 'stopped',
-	enable => 'true',
-}
-service {'ntpd':
-        ensure => 'running',
-        enable => 'true',
-}
-service { 'sshd':
-  ensure => 'running',
-  enable => 'true',
-}
-package {'firefox':
-ensure=> '52.8.0-1.el7.centos',
-}
-package {'htop':
-ensure => '2.2.0-1.el7',
-}
-package { 'mlocate':
-ensure => '0.26-8.el7';
-}
-package {'curl':
-ensure=> 'present',
-}
-package {'telnet':
-ensure => 'present',
-}
-package {'cowsay':
-ensure=> 'present',
-}
-package {'figlet':
-ensure=> 'present',
-}
-package {'fortune-mod':
-ensure => 'present',
-}
-package {'bind-utils':
-ensure => 'present',
-}
-
-file { '/home/satish':
-  ensure   => 'directory',
-  mode     => '755',
-  owner    => '550',
-}
-}
-
-node gitpractice-3 {
-
-file {'/etc/motd':
-        content => 'This is gitpractice 3 machine',
+	file {'/etc/motd':
+        content => ' This is gitpractice 2 system',
         }
-
-        service {'postfix':
-        ensure => 'running',
-        enable => 'true',
-}
-service {'ntpd':
-        ensure => 'running',
-        enable => 'true',
-}
-package {'firefox':
-ensure=> '52.8.0-1.el7.centos',
-}
-package {'htop':
-ensure => '2.2.0-1.el7',
-}
-package { 'mlocate':
-ensure => '0.26-8.el7';
-}
-package {'curl':
-ensure=> 'present',
-}
-package {'telnet':
-ensure => 'present',
-}
-package {'cowsay':
-ensure=> 'present',
-}
-package {'figlet':
-ensure=> 'present',
-}
-package {'fortune-mod':
-ensure => 'present',
-}
-package {'bind-utils':
-ensure => 'present',
-}
-}
-
-#$ puppet resource user luke
-    user { 'luke':
-     home => '/home/luke',
-     uid => '100',
-     ensure => 'present',
-     comment => 'Luke Kanies',
-     gid => '1000',
-     shell => '/bin/bash',
-     groups => ['adm','puppet']
-    }
-
-user { 'satish':
-	home => '/home/satish',
-	uid => '550',
-	ensure => 'present',
-comment => 'Satish Arya',
-gid => '1000',
-shell => '/bin/bash',
-groups => ['adm','puppet']
-}
-node gitpractice {
-
-file {'/etc/motd':
-        content => 'PuppetMaster',
+       
         }
+      
+     node gitpractice-3 {
+      
+       file {'/etc/motd':
+       content => 'This is gitpractice 3 machine',
+       }
+         
+       }
 
-        service {'postfix':
-        ensure => 'running',
-        enable => 'true',
-}
-service {'ntpd':
-        ensure => 'running',
-        enable => 'true',
-}
-package {'firefox':
-ensure=> '52.8.0-1.el7.centos',
-}
-package {'htop':
-ensure => '2.2.0-1.el7',
-}
-package { 'mlocate':
-ensure => '0.26-8.el7';
-}
-package {'curl':
-ensure=> 'present',
-}
-package {'telnet':
-ensure => 'present',
-}
-package {'cowsay':
-ensure=> 'present',
-}
-package {'figlet':
-ensure=> 'present',
-}
-package {'fortune-mod':
-ensure => 'present',
-}
-package {'bind-utils':
-ensure => 'present',
-}
-package {'tig.x86_64':
-ensure => 'present',
-}
-}
+      node gitpractice {
+    
+      file {'/etc/motd':
+      content => 'PuppetMaster',
+            }
+         
+       }
 
+      node puppetclient3 {
+    
+      file {'/etc/motd':
+      content => 'PuppetClient3',
+            }
+         
+       }
+
+ #$ puppet resource user luke
+      user { 'luke':
+      home => '/home/luke',
+      uid => '100',
+      ensure => 'present',
+      comment => 'Luke Kanies',
+      gid => '1000',
+      shell => '/bin/bash',
+      groups => ['adm','puppet']
+      }
+     
+      user { 'satish':
+      home => '/home/satish',
+      uid => '550',
+      ensure => 'present',
+      comment => 'Satish Arya',
+      gid => '1000',
+      shell => '/bin/bash',
+      groups => ['adm','puppet']
+      }
+      service {'postfix':
+       ensure => 'running',
+       enable => 'true',
+       }
+       service {'ntpd':
+       ensure => 'running',
+       enable => 'true',
+       }
+       service { 'sshd':
+       ensure => 'running',
+       enable => 'true',
+       }
+       package {'firefox':
+       ensure=> '52.8.0-1.el7.centos',
+       }
+       package {'htop':
+       ensure => '2.2.0-1.el7',
+       }
+       package { 'mlocate':
+       ensure => '0.26-8.el7';
+       }
+       package {'curl':
+       ensure=> 'present',
+       }
+       package {'telnet':
+       ensure => 'present',
+       }
+       package {'cowsay':
+       ensure=> 'present',
+       }
+       package {'figlet':
+       ensure=> 'present',
+       }
+       package {'fortune-mod':
+       ensure => 'present',
+       }
+       package {'bind-utils':
+       ensure => 'present',
+       }
